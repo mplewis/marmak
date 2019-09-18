f8821b20b12eefcc54408f9835e3044905706837
Matt Lewis <matt@mplewis.com>
2019-09-18 13:17:12 -0700

    feat: ensure bucket name set

--- api/src/storage.rb
+++ api/src/storage.rb
@@ -2,7 +2,9 @@ require 'google/cloud/storage'
 
 module Storage
   GOOGLE = Google::Cloud::Storage.new
-  BUCKET = GOOGLE.bucket ENV['BUCKET_NAME']
+  BUCKET_NAME = ENV['bucket_name']
+  raise 'BUCKET_NAME not set' unless BUCKET_NAME
+  BUCKET = GOOGLE.bucket BUCKET_NAME
 
   def Storage.upload_file(project, path, name)
     Storage::BUCKET.create_file path, "#{project}/#{name}"
